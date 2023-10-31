e781688bcec70af4ace447cb3d927570466f5cba
Bianca Fogaça <biancanfogaca@outlook.com>
2023-10-30 20:14:36 -0400

    repositorio, dominio, servico, fakeDB

--- Dominio/Aluno.java
+++ Dominio/Aluno.java
@@ -17,6 +17,53 @@ public class Aluno {
         this.senha = senha;
     }
 
-    // Getters e Setters
-    // ...
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(int periodo) {
+        this.periodo = periodo;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
+
+
